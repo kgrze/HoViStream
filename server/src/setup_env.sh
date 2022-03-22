5c41de537655d3e779fdb83d3c8088b4ad3a9e93
kgrze <grzeszczak.krzysztof@gmail.com>
2022-03-22 23:54:03 +0100

    Added HLS support via shaka-packager

--- server/src/setup_env.sh
+++ server/src/setup_env.sh
@@ -4,6 +4,10 @@ sudo apt install nginx-full
 sudo apt install ffmpeg
 sudo apt install gpac
 sudo apt install youtube-dl
+wget https://github.com/shaka-project/shaka-packager/releases/download/v2.6.1/packager-linux-x64
+
+sudo mv packager-linux-x64 /usr/local/bin/packager
+sudo chmod 755 /usr/local/bin/packager
 
 sudo chown -R $USER:$USER /etc/nginx
 sudo chmod 755 -R /etc/nginx/*
--- server/test/prepare_stream.sh
+++ server/test/prepare_stream.sh
@@ -3,7 +3,7 @@
 sudo rm -rf /var/www/media
 sudo mkdir -p /var/www/media/batman/stream
 
-cd /var/www/media/batman
+pushd /var/www/media/batman
 sudo youtube-dl --all-subs -o batman.mp4 'https://www.youtube.com/watch?v=mqqft2x_Aa4&ab_channel=WarnerBros.Pictures'
 
 # MPEG-DASH/HLS
@@ -28,16 +28,33 @@ sudo ffmpeg -y -i batman.mp4 -c:v libx264 \
 sudo ffmpeg -y -i batman.mp4 -map 0:1 -vn -c:a aac -b:a 128k -ar 48000 -ac 2 batman_audio.m4a
 
 sudo MP4Box -add batman.en-US.vtt:lang=en-US subtitle_en.mp4
+sudo ffmpeg -i batman.en-US.vtt -c:s webvtt batman_subs.vtt
 
-sudo MP4Box -dash 4000 -frag 4000 -rap \
--bs-switching no -profile onDemand  -fps 24 \
-batman_2160p.mp4#video:id=2160p \
-batman_1080p.mp4#video:id=1080p \
-batman_720p.mp4#video:id=720p \
-batman_audio.m4a#audio:id=English:role=main \
-subtitle_en.mp4:role=subtitle \
--out stream/batman.m3u8:dual
+# using MP4Box
+# sudo MP4Box -dash 4000 -frag 4000 -rap \
+# -bs-switching no -profile onDemand  -fps 24 \
+# batman_2160p.mp4#video:id=2160p \
+# batman_1080p.mp4#video:id=1080p \
+# batman_720p.mp4#video:id=720p \
+# batman_audio.m4a#audio:id=English:role=main \
+# subtitle_en.mp4:role=subtitle \
+# -out stream/batman.m3u8:dual
 
-sudo rm -f *.mp4
-sudo rm -f *.vtt
-sudo rm -f *.m4a
+#using shaka-packager
+pushd stream
+sudo packager \
+'in=../batman.mp4,stream=audio,init_segment=audio/init.mp4,segment_template=audio/$Number$.m4s' \
+'in=../batman_subs.vtt,stream=text,init_segment=text/init.mp4,segment_template=text/$Number$.m4s,dash_only=1' \
+'in=../batman_subs.vtt,stream=text,segment_template=text/$Number$.vtt,hls_only=1' \
+'in=../batman_720p.mp4,stream=video,init_segment=h264_720p/init.mp4,segment_template=h264_720p/$Number$.m4s' \
+'in=../batman_1080p.mp4,stream=video,init_segment=h264_1080p/init.mp4,segment_template=h264_1080p/$Number$.m4s' \
+'in=../batman_2160p.mp4,stream=video,init_segment=h264_2160p/init.mp4,segment_template=h264_2160p/$Number$.m4s' \
+--generate_static_live_mpd --mpd_output batman_dash.mpd \
+--hls_master_playlist_output batman_hls.m3u8
+popd
+
+sudo rm -f ./*.mp4
+sudo rm -f ./*.vtt
+sudo rm -f ./*.m4a
+
+popd
