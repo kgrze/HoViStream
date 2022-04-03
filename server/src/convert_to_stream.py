02db6fb582e1b91e9b303a80a200fe657ca2a37a
kgrze <grzeszczak.krzysztof@gmail.com>
2022-04-03 23:50:35 +0200

    Added ffmpeg resacaling and packaging via shaka-packager

--- server/src/convert_to_stream.py
+++ server/src/convert_to_stream.py
@@ -4,49 +4,101 @@ import os
 import sys
 import subprocess
 
-def ffmpeg_encode_video(path_input_video, path_output, scale):
-    cmd = ['sudo', 'ffmpeg']
+def ffmpeg_convert_video(path_input_video, path_output, scale):
+    if scale in [1280, 1920, 2360]:
+        cmd = ['ffmpeg']
+        cmd.append('-y')
+        cmd.append('-i')
+        cmd.append(path_input_video)
+        cmd.append('-c:v')
+        cmd.append('libx264')
+        cmd.append('-r')
+        cmd.append('24')
+        cmd.append('-x264opts')
+        cmd.append('keyint=48:min-keyint=48:no-scenecut')
+        cmd.append('-vf')
+        cmd.append('scale='+str(scale)+':-2')
+        cmd.append('-maxrate')
+        cmd.append('4000K')
+        cmd.append('-movflags')
+        cmd.append('faststart')
+        cmd.append('-bufsize')
+        cmd.append('8600K')
+        cmd.append('-profile:v')
+        cmd.append('main')
+        cmd.append('-preset')
+        cmd.append('slow')
+        cmd.append('-an')
+        # cmd.append('-threads')
+        # cmd.append('4')
+        out_name = 'video_'+str(scale)+'p'+'.mp4'
+        output = os.path.join(path_output, out_name)
+        cmd.append(output)
+        subprocess.call(cmd)
+        subprocess.call(['chmod', '755', output])
+        return output
+
+def ffmpeg_extract_audio(path_input_video, path_output):
+    cmd = ['ffmpeg']
     cmd.append('-y')
     cmd.append('-i')
     cmd.append(path_input_video)
-    cmd.append('-c:v')
-    cmd.append('libx264')
-    cmd.append('-r')
-    cmd.append('24')
-    cmd.append('-x264opts')
-    cmd.append('keyint=48:min-keyint=48:no-scenecut')
-    cmd.append('-vf')
-    cmd.append('scale=-2:'+scale)
-    cmd.append('-maxrate')
-    cmd.append('6000K')
-    cmd.append('-movflags')
-    cmd.append('faststart')
-    cmd.append('-bufsize')
-    cmd.append('8600K')
-    cmd.append('-profile:v')
-    cmd.append('main')
-    cmd.append('-preset')
-    cmd.append('fast')
-    cmd.append('-an')
-    output = os.path.join(path_output, 'video_'+scale+'p'+'.mp4')
+    cmd.append('-map')
+    cmd.append('0:1')
+    cmd.append('-vn')
+    cmd.append('-c:a')
+    cmd.append('aac')
+    cmd.append('-b:a')
+    cmd.append('128k')
+    cmd.append('-ar')
+    cmd.append('48000')
+    cmd.append('-ac')
+    cmd.append('2')
+    out_name = 'audio.m4a'
+    output = os.path.join(path_output, out_name)
     cmd.append(output)
-
-    print(cmd)
     subprocess.call(cmd)
+    subprocess.call(['chmod', '755', output])
+    return output
+
+def shaka_pack_to_dash_hls(path_1280, path_1920, path_2360, path_audio, path_output):
+    cmd = ['packager']
+    cmd.append('in='+path_audio+',stream=audio,init_segment=audio/init.mp4,segment_template=audio/$Number$.m4s')
+    cmd.append('in='+path_1280+',stream=video,init_segment=h264_1280/init.mp4,segment_template=h264_1280/$Number$.m4s')
+    cmd.append('in='+path_1920+',stream=video,init_segment=h264_1920/init.mp4,segment_template=h264_1920/$Number$.m4s')
+    cmd.append('in='+path_2360+',stream=video,init_segment=h264_2360/init.mp4,segment_template=h264_2360/$Number$.m4s')
+    cmd.append('--generate_static_live_mpd')
+    cmd.append('--mpd_output')
+    cmd.append(os.path.join(path_output, 'stream_dash.mpd'))
+    cmd.append('--hls_master_playlist_output')
+    cmd.append(os.path.join(path_output, 'stream_hls.m3u8'))
+    subprocess.call(cmd, cwd=path_output)
+
+def conv_to_stream(path_input_video, path_output_stream_location):
+    if os.path.exists(path_input_video) is False:
+        print('Specified video file path: {} does not exist'.format(path_input_video))
+        exit(-1)
+    if os.path.exists(path_output_stream_location) is False:
+        print('Specified output path: {} does not exist. Creating it'.format(path_output_stream_location))
+        os.makedirs(path_output_stream_location)
+
+    path_video_1280 = ffmpeg_convert_video(path_input_video, path_output_stream_location, 1280)
+    path_video_1920 = ffmpeg_convert_video(path_input_video, path_output_stream_location, 1920)
+    path_video_2360 = ffmpeg_convert_video(path_input_video, path_output_stream_location, 2360)
+    path_audio = ffmpeg_extract_audio(path_input_video, path_output_stream_location)
+
+    shaka_pack_to_dash_hls(path_video_1280, path_video_1920, path_video_2360, path_audio, path_output_stream_location)
+
+    os.remove(path_video_1280);
+    os.remove(path_video_1920);
+    os.remove(path_video_2360);
+    os.remove(path_audio);
 
 if len(sys.argv) < 3:
     print('Please specify paths to input video file and output stream location')
     sys.exit()
 
-path_input_video  = sys.argv[1]
-if os.path.exists(path_input_video) is False:
-    print('Specified video file path: {} does not exist'.format(path_input_video))
-
-path_output  = sys.argv[2]
-if os.path.exists(path_output) is False:
-    print('Specified output path: {} does not exist'.format(path_output))
-
-ffmpeg_encode_video(path_input_video, path_output, '720')
+conv_to_stream(sys.argv[1], os.path.abspath(sys.argv[2]))
 
 #python convert_to_stream.py ~/Videos/House.Of.Gucci.2021.1080p.AMZN.WEBRip.DDP5.1.Atmos.x264-TEPES/HOG.mp4 .
 
