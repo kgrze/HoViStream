022a463e500fa955c26d019983ef85cb5909f48e
kgrze <grzeszczak.krzysztof@gmail.com>
2022-04-08 19:11:09 +0200

    Added HTML templates for list of movies and movie specific sites

--- server/src/refresh_server.sh
+++ server/src/refresh_server.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 sudo yes | sudo cp -rf ../data/nginx.conf /etc/nginx/nginx.conf
-cp -rf ../data/web/index.html /home/kgrze/HoViStream/www/html/
 sudo systemctl reload nginx.service
 echo "HoViStream server data refreshed"
--- server/test/prepare_stream.sh
+++ server/test/prepare_stream.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
 rm -rf /home/kgrze/HoViStream/media
-mkdir -p /home/kgrze/HoViStream/media/batman
 
+cp -rf ../data/web/index.html /home/kgrze/HoViStream/www/html/
+
+mkdir -p /home/kgrze/HoViStream/media/batman
 pushd /home/kgrze/HoViStream/media/batman
 youtube-dl --all-subs -o batman.mp4 'https://www.youtube.com/watch?v=mqqft2x_Aa4&ab_channel=WarnerBros.Pictures'
 popd
-
 python ../src/convert_to_stream.py /home/kgrze/HoViStream/media/batman/batman.mp4 /home/kgrze/HoViStream/www/stream/batman
-
+cp -rf ../data/web/batman.html /home/kgrze/HoViStream/www/html/
 rm -rf /home/kgrze/HoViStream/media/batman
+
+mkdir -p /home/kgrze/HoViStream/media/topgun
+pushd /home/kgrze/HoViStream/media/topgun
+youtube-dl --all-subs -o topgun.mp4  'https://www.youtube.com/watch?v=giXco2jaZ_4&ab_channel=ParamountPictures'
+popd
+python ../src/convert_to_stream.py /home/kgrze/HoViStream/media/topgun/topgun.mp4 /home/kgrze/HoViStream/www/stream/topgun
+cp -rf ../data/web/topgun.html /home/kgrze/HoViStream/www/html/
+rm -rf /home/kgrze/HoViStream/media/topgun
 
 ../src/refresh_server.sh
 echo "HoViStream sever is ready."
