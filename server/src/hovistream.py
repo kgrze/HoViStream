6307d63644319f06fa24397f0fb621c60520e1ea
kgrze <grzeszczak.krzysztof@gmail.com>
2022-04-11 00:40:40 +0200

    Initial main script hovistream.py for orchestrating streaming server

---
+++ server/src/hovistream.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import mimetypes
+
+path_in_media = '/home/kgrze/HoViStream/media'
+path_out_stream = '/home/kgrze/HoViStream/www/stream'
+
+def is_video_file(path):
+    """Check file extension to detect valid video file"""
+    fileExtension = path.rsplit('.', 1)
+    if fileExtension[1] not in ['avi', 'mp4', 'mov', 'mkv', 'webm']:
+        return False
+    return True
+
+def is_subtitles_file(path):
+    """Check file extension to detect valid subtitle file"""
+    for root, _, items in os.walk(path):
+        for item in items:
+            file_path = os.path.join(root, item)
+            file_ext = file_path.rsplit('.', 1)
+            if file_ext[1] in ['srt', 'vtt']:
+                return file_path
+    return None
+
+list_video = []
+for root, _, items in os.walk(path_in_media):
+    for item in items:
+        video_item = []
+        file_path = os.path.join(root, item)
+        if is_video_file(file_path):
+            vide_title = os.path.basename(file_path).split('.')[0]
+            video_item.append(vide_title)
+            video_item.append(file_path)
+            path_subs = is_subtitles_file(root)
+            if path_subs is not None:
+                video_item.append(path_subs)
+            list_video.append(video_item)
+print(list_video)
