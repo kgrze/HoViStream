73700dfac684bbaa2722095eff00d80798041a8d
kgrze <grzeszczak.krzysztof@gmail.com>
2022-04-20 00:38:22 +0200

    Added coloring to setup script

--- server/src/setup.sh
+++ server/src/setup.sh
@@ -14,11 +14,8 @@ wget https://github.com/shaka-project/shaka-packager/releases/download/v2.6.1/pa
 sudo mv packager-linux-x64 /usr/local/bin/packager
 sudo chmod 755 /usr/local/bin/packager
 
-./refresh_server.sh
-
-echo 'HoViStream initialization finished.'
-echo '1. Copy movies to /home/$USER/HoViStream/media'
-echo '2. Run hovistream.py script'
-echo '3. Run "ip addr show" to see what is Your IP address'
-echo '4. Open web browser and type "http://<your IP address>" and You should see the list of movies available for local streaming.'
-echo 'Enjoy !!!'
+echo -e '\e[31mHoViStream initialization finished.\e[0m'
+echo -e '\e[31m1. Run hovistream.py {input_path_to_movie_folder} {output_path_to_www_stream_content}\e[0m'
+echo -e '\e[31m2. Run "ip addr show" to see what is Your IP address\e[0m'
+echo -e '\e[31m3. Open web browser and type "http://<your IP address>" and You should see the list of movies available for local streaming.\e[0m'
+echo -e '\e[31m4. Enjoy !!!\e[0m'
